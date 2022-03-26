1f338dc5d6f33980bdd25faf37b7c64f476a6d4b
bronz3beard <info2rory@gmail.com>
2022-03-26 11:44:10 +1000

    update links for demo

--- src/components/iconsRow/index.js
+++ src/components/iconsRow/index.js
@@ -6,16 +6,16 @@ const IconsRow = () => {
     return (
         <div className="w-52 h-16 flex space-x-10 justify-center items-center bg-white rounded-br-xl">
             <a
-                // target="_blank"
-                // rel="noopener noreferrer"
-                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/bronz3beard/react-hud"
             >
                 <FontAwesomeIcon size="2x" color="#77B244" icon={faGitSquare} />
             </a>
             <a
-                // target="_blank"
-                // rel="noopener noreferrer"
-                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.npmjs.com/package/react-heads-up-display"
             >
                 <FontAwesomeIcon size="2x" color="#77B244" icon={faNpm} />
             </a>
